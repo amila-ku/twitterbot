3b77597b03f2c3a6594a68b15429a35a63191236
amilaku <amila.kumaranayaka@gmail.com>
2019-09-28 02:28:22 +0200

    using streams

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"github.com/ChimeraCoder/anaconda"
+	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -19,9 +21,26 @@ func main() {
 	anaconda.SetConsumerSecret(consumerSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
 
-	res, _ := api.GetSearch("golang", nil)
-	fmt.Print(res)
-	for _, tweet := range res.Statuses {
-		fmt.Print(tweet.Text)
+	stream := api.PublicStreamFilter(url.Values{
+		"track": []string{"#world"},
+	})
+
+	defer stream.Stop()
+
+	for v := range stream.C {
+		fmt.Println("starting")
+
+		t, ok := v.(anaconda.Tweet)
+
+		if !ok {
+			logrus.Warningf("%T", v)
+		}
+		fmt.Printf("%s\n", t.Text)
 	}
+
+	// res, _ := api.GetSearch("golang", nil)
+	// fmt.Print(res)
+	// for _, tweet := range res.Statuses {
+	// 	fmt.Print(tweet.Text)
+	// }
 }
